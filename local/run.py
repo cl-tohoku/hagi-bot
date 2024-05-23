d154ac33dc200a1f9f458dbf677f91b547a68a81
RyoheiKamei <ryohei.kamei.s4@dc.tohoku.ac.jp>
2024-05-23 14:05:38 +0900

    updated run.py

--- local/run.py
+++ local/run.py
@@ -69,52 +69,52 @@ def main(args) -> None:
         uttr = input("ユウキ: ")
         user_content = uttr
 
-        # メインモデルの応答時間の計測開始
+        # Start measuring the response time of the main model
         start_time = time.time()
 
-        # ユーザが "q" あるいは "exit" と入力したらループを抜ける (DEBUG)
+        # Exit the loop when the user inputs "q" or "exit" (DEBUG)
         if args.debug:
             if user_content in ["exit", "q"]:
                 break
 
-        # ユーザの入力をメインモデルに渡す
+        # Pass user input to the main model
         main_client.add_user_input(user_content)
 
-        # メインモデルの応答を要求し、応答を出力する
-        debug_print(args, "メインモデルへのリクエスト開始")
+        # Requests a response from the main model and outputs a response
+        debug_print(args, "メインモデルへのリクエスト開始")  # "Start of request to main model"
         if args.stream:
             for sentence in main_client.request_and_print_assistant_response_stream():
-                print(f"シズカ: {sentence}")
+                print(f"シズカ: {sentence}")  # "Shizuka:"
 
         else:
             main_client.request_and_print_assistant_response()
 
-        # メインモデルの応答時間の計測終了
+        # End of main model response time measurement
         end_time = time.time()
         elapsed_time = end_time - start_time
 
-        # メインモデルの応答時間を出力する (DEBUG)
+        # Output main model response time (DEBUG)
         debug_print(args, f"elapsed_time: {elapsed_time:.2f} sec")
 
-        # スロットコンテンツモデルの応答時間の計測開始
+        # Start measuring the response time of the slot content model
         start_time = time.time()
 
-        # スロットコンテンツモデルに渡す対話履歴を取得する
+        # Obtain dialogue history to be passed to the slot content model
         main_chat_history = main_client.chat_history
 
-        # スロットコンテンツモデルに渡す対話履歴を出力する (DEBUG)
+        # Output dialogue history to be passed to the slot content model (DEBUG)
         # debug_print(args, f"main_chat_history: {main_chat_history}")
 
-        # メインモデルの対話履歴をスロットコンテンツモデルに渡す
+        # Passing the dialogue history of the main model to the slot content model
         for slot in Slot:
-            # スロットコンテンツモデルに最新の対話履歴を入力として渡す
+            # Passing the latest dialogue history as input to the slot content model
             slot_clients[slot].add_main_chat_history_as_user_input(
                 main_chat_history=main_chat_history,
                 enable_fewshot=args.enable_fewshot
             )
 
-        # スロットコンテンツモデルの応答を要求する
-        debug_print(args, "スロットモデルへのリクエスト開始")
+        # Request response to slot content model.
+        debug_print(args, "スロットモデルへのリクエスト開始") # "Start of request to slot model"
         asyncio.run(
             await_all(
                 [
@@ -125,61 +125,61 @@ def main(args) -> None:
             )
         )
 
-        # スロットコンテンツモデルの応答を受け取る
+        # Receive slot content model response
         slot_contents = {
             slot: slot_client.slot_content
             for slot, slot_client
             in slot_clients.items()
         }
 
-        # スロットコンテンツモデルの応答時間の計測終了
+        # End of measurement of response times for slot content models
         end_time = time.time()
         elapsed_time = end_time - start_time
 
-        # スロットコンテンツモデルの応答時間を出力する (DEBUG)
+        # Output response time of slot content model (DEBUG)
         debug_print(args, f"elapsed_time: {elapsed_time:.2f} sec")
 
-        # スロットコンテンツモデルの応答を出力する (DEBUG)
+        # Output slot content model response (DEBUG)
         slot_contents_str = ", ".join([f"{slot.text}: {content}" for slot, content in slot_contents.items()])
         debug_print(args, f"slot_contents: {{{slot_contents_str}}}")
 
-        # メインモデルの保持するスロットコンテンツを更新する
+        # Update slot content held by the main model.
         main_client.slot_contents = slot_contents
 
-# --stream の default をstreamモードにしたので、引数で指定する必要なし（指定するとむしろstreamモードじゃ無くなる）
+
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--main-model", type=str, default="gpt-3.5-turbo")  # default はGPT3.5
-    parser.add_argument("--slot-model", type=str, default="gpt-3.5-turbo")  # default はGPT3.5
+    parser.add_argument("--main-model", type=str, default="gpt-3.5-turbo")  # default: GPT3.5
+    parser.add_argument("--slot-model", type=str, default="gpt-3.5-turbo")  # default: GPT3.5
     parser.add_argument("--main-histrory-length", type=int, default=None)
     parser.add_argument("--slot-input-histrory-length", type=int, default=None)
     parser.add_argument("--debug", action="store_true")
     parser.add_argument("--disable-warning", action="store_true")
     parser.add_argument("--enable-fewshot", action="store_true")
     parser.add_argument("--stream", action="store_true")
-    parser.add_argument("--max_time", type=float, default=290.0) # 制限時間の追加
-    parser.add_argument("--time_out", type=float, default=2.0) # user input のタイムアウト追加
-    parser.add_argument("--is_time_out", action="store_false") # user input のタイムアウトのスイッチ
+    parser.add_argument("--max_time", type=float, default=290.0) # add max time
+    parser.add_argument("--time_out", type=float, default=2.0) # add user input time out
+    parser.add_argument("--is_time_out", action="store_false") # add user input time out switch
     parser.add_argument("--main-model-timeout", type=float, default=10)
     parser.add_argument("--slot-model-timeout", type=float, default=10)
     parser.add_argument("--main-model-max-retry", type=int, default=5)
     parser.add_argument("--slot-model-max-retry", type=int, default=3)
     parser.add_argument("--max-tokens", type=int, default=256)
-    # parser.add_argument("--ending-bias", type=int, default=10)  # 出力を短くしますが、API 側のバグで利用できません。
+    # parser.add_argument("--ending-bias", type=int, default=10)  # Shortens the output, but is not available due to a bug on the API side
 
     args = parser.parse_args()
 
     if args.main_model != "gpt-4":
-        warn_print(args, "gpt-4 以前のモデルでは表情が変化しない可能性があります。")
+        warn_print(args, "gpt-4 以前のモデルでは表情が変化しない可能性があります。")  # "Models before gpt-4 may not change facial expressions."
     if args.slot_model != "gpt-4":
-        warn_print(args, "gpt-4 以前のモデルではスロットの出力が安定しません。安定には few-shot prompt の追加が必要です。")
+        warn_print(args, "gpt-4 以前のモデルではスロットの出力が安定しません。安定には few-shot prompt の追加が必要です。")  # "Models before gpt-4 may not stabilize the output of the slot. Adding a few-shot prompt is necessary for stabilization."
         if args.enable_fewshot:
             counts = get_fewshot_messages_counts()
             counts_str = ", ".join([f"{slot.text}: {count}件" for slot, count in counts.items()])
             count_sum = sum(counts.values())
-            warn_print(args, f"現在登録されている few-shot の対話文は {counts_str} の計{count_sum}件です。")
+            warn_print(args, f"現在登録されている few-shot の対話文は {counts_str} の計{count_sum}件です。")  # "The number of registered few-shot dialogues is currently {counts_str}, totaling {count_sum}."
         else:
-            warn_print(args, "few-shot prompt を有効にするには `--enable-fewshot` を指定してください。")
+            warn_print(args, "few-shot prompt を有効にするには `--enable-fewshot` を指定してください。")  # "To enable few-shot prompt, specify `--enable-fewshot`."
 
     return args
 
